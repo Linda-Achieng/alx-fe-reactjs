9666bc9755596f9ea6e3fc75c69a4c97330887f1
Linda Achieng <achienglinda555@gmail.com>
2024-08-18 19:19:54 +0300

    modified usercontext.js and userprofile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,15 +1,11 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../UserContext';
-
-function UserProfile() {
-  const userData = useContext(UserContext);
-
+const UserProfile = (props) => {
   return (
     <div>
-      <p>Name: {userData.name}</p>
-      <p>Email: {userData.email}</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
-}
+};
 
 export default UserProfile;
