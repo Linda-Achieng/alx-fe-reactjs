d7ab703253363651829a24b6c31ae005eea29302
Linda Achieng <achienglinda555@gmail.com>
2024-09-01 01:53:03 +0300

    moved react-todo to _tests_

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,33 @@
+// src/__tests__/TodoList.test.js
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import TodoList from '../components/TodoList';
+
+test('renders TodoList component', () => {
+  render(<TodoList />);
+  expect(screen.getByText(/Todo List/i)).toBeInTheDocument();
+  expect(screen.getByPlaceholderText(/Add a new todo/i)).toBeInTheDocument();
+});
+
+test('adds a new todo', () => {
+  render(<TodoList />);
+  fireEvent.change(screen.getByPlaceholderText(/Add a new todo/i), { target: { value: 'New Todo' } });
+  fireEvent.click(screen.getByText(/Add Todo/i));
+  expect(screen.getByText(/New Todo/i)).toBeInTheDocument();
+});
+
+test('toggles a todo', () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText(/Learn React/i);
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle('text-decoration: line-through');
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle('text-decoration: none');
+});
+
+test('deletes a todo', () => {
+  render(<TodoList />);
+  fireEvent.click(screen.getByText(/Delete/i));
+  expect(screen.queryByText(/Learn React/i)).toBeNull();
+});
