a83fd74a43639b0f00dabf657ed3400c348d9e55
Linda Achieng <achienglinda555@gmail.com>
2024-09-06 01:06:11 +0300

    modified tailwind-react-intergration

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -2,9 +2,10 @@
 import { defineConfig } from 'tailwindcss';
 
 export default defineConfig({
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Replace 'purge'
   theme: {
-    extend: {},
+    extend: {}, // Use 'extend' to add custom styles
   },
-  plugins: [],
+  darkMode: 'media', // Can be 'media' or 'class' (or false to disable dark mode)
+  plugins: [], // Add any plugins you need
 });
