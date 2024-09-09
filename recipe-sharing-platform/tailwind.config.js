0bc9b14ab225392846b3098e6fb7cfbe826c75bc
Linda Achieng <achienglinda555@gmail.com>
2024-09-09 15:02:26 +0300

    modified  recipe-sharing-platform

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,9 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
